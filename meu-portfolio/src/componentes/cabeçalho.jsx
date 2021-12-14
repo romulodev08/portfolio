12eed2d6eb45fe1c1757e85d744d675e56dfe977
Rômulo Martins <romulodev08@gmail.com>
2021-12-14 14:28:48 -0300

    cabeçalho criado

--- meu-portfolio/src/componentes/cabeçalho.jsx
+++ meu-portfolio/src/componentes/cabeçalho.jsx
@@ -1,16 +1,24 @@
 import { Component } from "react";
+import "../estilo/cabeçalho.css"
 import { Link } from "react-router-dom"
-
 export default class Cabeçalho extends Component {
     render() {
-        return(
+        return (
             <header>
-                <div id="logo"></div>
-                <nav>
-                    <Link className="link" to="/" >Início</Link>
-                    <Link className="link" to="projetos" >Projetos</Link>
-                    <Link className="link" to="contato" >Contato</Link>
-                </nav>
+                <h1>Título</h1>
+                <div className="menu">
+                    <label for="inputBotaomenu" id="botaolabel">
+                        <div className="trace"></div>
+                        <div className="trace"></div>
+                        <div className="trace"></div>
+                    </label>
+                    <input type="checkbox" name="inputBotaomenu" id="inputBotaomenu" />
+                    <nav id="navegação">
+                        <Link className="link" to="/" >Início</Link>
+                        <Link className="link" to="projetos" >Projetos</Link>
+                        <Link className="link" to="contato" >Contato</Link>
+                    </nav>
+                </div>
             </header>
         )
     }
--- meu-portfolio/src/componentes/inicial.jsx
+++ meu-portfolio/src/componentes/inicial.jsx
@@ -1,8 +1,8 @@
 
 export default function Inicial(props) {
     return(
-        <div>
-            página inicial
-        </div>
+        <section>
+            
+        </section>
     )
 }
