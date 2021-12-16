95ae7e4d8daaba22f780f1c1e611ac37f602ae9e
Rômulo Martins <romulodev08@gmail.com>
2021-12-16 12:23:39 -0300

    saudação finalizada
    código para dar bom dia, boa tarde ou boa noite de acordo com o horário do dispositivo

--- meu-portfolio/src/componentes/inicial.jsx
+++ meu-portfolio/src/componentes/inicial.jsx
@@ -1,4 +1,7 @@
 import { Component } from "react"
+
+import "../estilo/inicial.css"
+
 import FotoDePerfil from "../imagens/foto-do-currículo.jpeg"
 import Curriculo from "../documentos/currículoFrontEndJrRom-5.pdf"
 export default class Inicial extends Component {
@@ -8,16 +11,29 @@ export default class Inicial extends Component {
             saudação: "Olá"
         }
     }
-    
+    componentDidMount() {
+        const saudar = ["Bom dia", "Boa tarde", "Boa noite"]
+        setInterval(() => {
+            const hora = new Date().getHours()
+            if(hora >= 6 && hora < 12) {
+                this.setState({saudação: saudar[0]})
+            } else if(hora >= 12 && hora < 18) {
+                this.setState({saudação: saudar[1]})
+            } else if(hora >= 18 || hora < 6) {
+                this.setState({saudação: saudar[2]})
+            }
+        }, 1000);
+    };
+
     render() {
-        return(
+        return (
             <div>
-                <section>
-                    <p>{ this.state.saudação }, meu nome é</p>
+                <section className="saudação">
+                    <p>{this.state.saudação}, meu nome é</p>
                     <h2>Rômulo Martins</h2>
                     <p>sou desenvolvedor web Front End</p>
                 </section>
-                <section>
+                <section className="sobre">
                     <h2>Sobre</h2>
                     <div>
                         <img src={FotoDePerfil} alt="" />
