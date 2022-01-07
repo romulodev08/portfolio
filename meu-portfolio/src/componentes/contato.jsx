e7a6c6142f15bfb18dc14ad724d937978d216b4b
Rômulo Martins <romulodev08@gmail.com>
2022-01-07 11:55:52 -0300

    link do linkedin adicionado aos contatos

--- meu-portfolio/src/componentes/contato.jsx
+++ meu-portfolio/src/componentes/contato.jsx
@@ -10,6 +10,10 @@ export default function Contato(props) {
                 <span className="contatoItem">romulodev08@gmail.com</span>
             </div>
             <div>
+                <label className="contatoItem">LinkedIn:</label>
+                <span className="contatoItem"><a href="https://www.linkedin.com/in/r%C3%B4mulo-martins-40b962210/" rel="noopener noreferrer" target="_blank">/romulo-martins</a></span>
+            </div>
+            <div>
                 <label className="contatoItem">Telefone:</label>
                 <span className="contatoItem">(85)9 9289 1479</span>
                 <span className="contatoItem">/ (85)9 9261 7961</span>
